9a41a2a04d3f2aed10891c0fd70bb4eb68ad3bbf
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-05-08 21:57:51 +0100

    Project setup

---
+++ FaceTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceTracker
+//
+//  Created by Anurag Ajwani on 08/05/2019.
+//  Copyright © 2019 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
